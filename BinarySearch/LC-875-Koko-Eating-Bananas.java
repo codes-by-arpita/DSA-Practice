2f9ec25f3c26629bc20e2029092be02b754a2989
Arpita Padhi <arpita.padhi18@gmail.com>
2026-04-20 22:58:58 +0530

    Solved LC-875. Koko Eating Bananas

---
+++ BinarySearch/LC-875-Koko-Eating-Bananas.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int high=0,low=1;
+        for(int i:piles){
+            high = Math.max(high,i);
+        }
+
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            long Th=0;
+            for(int p:piles){
+                Th+=(p+mid-1)/mid;  
+            }
+            if(Th<=h){
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
+        }
+        return low;
+    }
+}
