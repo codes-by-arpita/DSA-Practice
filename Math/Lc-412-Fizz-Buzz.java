f005a149736fbd4e1dd7376a756210a2ec2e3bf9
Arpita Padhi <arpita.padhi18@gmail.com>
2026-04-23 23:01:55 +0530

    Solved Lc-412 Fizz Buzz

---
+++ Math/Lc-412-Fizz-Buzz.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> ans = new ArrayList<>();
+
+        for (int i = 1; i <= n; i++) {
+            String s = "";
+
+            if (i % 3 == 0) s += "Fizz";
+            if (i % 5 == 0) s += "Buzz";
+
+            if (s.equals("")) {
+                ans.add(String.valueOf(i));
+            } else {
+                ans.add(s);
+            }
+        }
+
+        return ans;
+    }
+}
