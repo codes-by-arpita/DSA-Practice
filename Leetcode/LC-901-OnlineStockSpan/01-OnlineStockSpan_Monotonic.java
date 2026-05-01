a1189f812d63ca81682161dc778b0de169e0e0f0
arpita padhi <arpita.padhi18@gmail.com>
2026-05-01 13:17:36 +0530

    Online Stock Span

---
+++ Leetcode/LC-901-OnlineStockSpan/01-OnlineStockSpan_Monotonic.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class StockSpanner {
+    Stack<int[]> st;
+    public StockSpanner() {
+        st=new Stack<>();
+    }
+    
+    public int next(int price) {
+        int span=1;
+
+        while(!st.isEmpty() && price >= st.peek()[0]){
+            span+= st.pop()[1];
+        }
+        st.push(new int[]{price,span});
+        return span;
+    }
+}
