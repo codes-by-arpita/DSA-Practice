b404db89ddc57fd8d9f611a2c0a7ece636c38b22
arpita padhi <arpita.padhi18@gmail.com>
2026-05-03 00:14:39 +0530

    TwoSumII

---
+++ Leetcode/LC-167-TwoSumII-Input Array Is Sorted/03-TwoSumII_BinarySearch.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        for(int i = 0;i<numbers.length;i++){
+            int left=i+1 , need = target-numbers[i], right=numbers.length-1;
+
+            while(left <= right){
+                int mid=left+(right-left)/2;
+
+                if(need==numbers[mid]){
+                    return new int[]{i+1,mid+1};
+                }else if(need>numbers[mid]){
+                    left=mid+1;
+                }else{
+                    right=mid-1;
+                }
+            }
+        }
+        return new int[]{-1,-1};
+    }
+}
