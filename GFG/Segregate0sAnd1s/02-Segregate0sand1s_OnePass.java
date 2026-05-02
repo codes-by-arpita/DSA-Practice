24f16e45b555f8394346ffc947c1e6d5c91219b8
arpita padhi <arpita.padhi18@gmail.com>
2026-05-03 01:20:02 +0530

    Segregate 0s and 1s

---
+++ GFG/Segregate0sAnd1s/02-Segregate0sand1s_OnePass.java
@@ -0,0 +1,14 @@
+class Solution {
+    void segregate0and1(int[] arr) {
+      int zero=0;
+      
+      for(int i=0;i<arr.length;i++){
+          if(arr[i]==0){
+              int temp=arr[i];
+              arr[i]=arr[zero];
+              arr[zero]=temp;
+              zero++;
+          }
+      }
+    }
+}
