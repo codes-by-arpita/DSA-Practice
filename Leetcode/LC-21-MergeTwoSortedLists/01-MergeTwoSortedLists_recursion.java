d232463bb3df93123215edf24e83828052988730
arpita padhi <arpita.padhi18@gmail.com>
2026-05-09 22:29:09 +0530

    MergeTwoSortedLists

--- Leetcode/LC-21-MergeTwoSortedLists/01-MergeTwoSortedLists_recursion.java
+++ Leetcode/LC-21-MergeTwoSortedLists/01-MergeTwoSortedLists_recursion.java
@@ -1,3 +1,18 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {}
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         if (list1 == null) return list2;
