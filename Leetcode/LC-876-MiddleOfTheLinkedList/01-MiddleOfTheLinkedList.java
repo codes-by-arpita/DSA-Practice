7585124e5987f869860f3763556944c562a5820e
arpita padhi <arpita.padhi18@gmail.com>
2026-05-09 22:30:10 +0530

    MiddleOfTheLinkedList

--- Leetcode/LC-876-MiddleOfTheLinkedList/01-MiddleOfTheLinkedList.java
+++ Leetcode/LC-876-MiddleOfTheLinkedList/01-MiddleOfTheLinkedList.java
@@ -1,3 +1,18 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {}
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
 class Solution {
     public ListNode middleNode(ListNode head) {
         ListNode fast=head;
