e1e8e7a569fde439278316fbf4db5594de72d220
arpita padhi <arpita.padhi18@gmail.com>
2026-05-30 02:36:44 +0530

    LineTrip

---
+++ CodeForces-TLE/02-A-LineTrip/Main.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int n=sc.nextInt();
+            int x=sc.nextInt();
+            int a[]=new int[n];
+            for(int i=0;i<n;i++){
+                a[i]=sc.nextInt();
+            }
+            int maxgap=a[0];
+            for(int i=1;i<n;i++){
+                maxgap=Math.max(maxgap,a[i]-a[i-1]);
+            }
+            maxgap=Math.max(maxgap,2*(x-a[n-1]));
+            System.out.println(maxgap);
+        }
+    }
+}
