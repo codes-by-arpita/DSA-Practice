3684ac5d2dabf102ed0baacccac87337793a8ce0
Arpita <arpita.padhi18@gmail.com>
2026-07-07 23:39:13 +0530

    SumOfIntegersWithMaximumDigitRange

--- Leetcode/LC-3751-TotalWavinessOfNumbersInRangeI/01-TotalWavinessOfNumbersInRangeI.java
+++ Leetcode/LC-3751-TotalWavinessOfNumbersInRangeI/01-TotalWavinessOfNumbersInRangeI.java
@@ -4,6 +4,7 @@ class Solution {
         for (int i = num1 ; i<= num2 ; i++){
             String no = i+"";
             for(int j =1 ; j< no.length()-1;j++){
+                
                 if(no.charAt(j)>no.charAt(j-1)&&no.charAt(j)>no.charAt(j+1))count++;
                 if(no.charAt(j)<no.charAt(j-1)&&no.charAt(j)<no.charAt(j+1))count++;
             }
